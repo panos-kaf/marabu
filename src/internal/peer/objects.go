1c96bacc5f92c1b6a364acd4b56dbad8581d6aca
Panos Kaf <panoskatsalifis@gmail.com>
2026-05-05 17:26:41 +0300

    add valid tx to mempool

--- src/internal/peer/objects.go
+++ src/internal/peer/objects.go
@@ -26,6 +26,11 @@ func (p *Peer) acceptObject(msgType types.MessageType, obj types.Object, result 
 	}
 	p.logInfo(fmt.Sprintf("Successfully processed and stored %s", result.ObjID))
 
+	
+	if tx, ok := obj.(*types.Transaction); ok {
+		p.Manager.AddToMempool(tx, result.Fee)
+	}
+	
 	// Gossip
 	BroadcastIHaveObject(result.ObjID)
 
@@ -34,22 +39,23 @@ func (p *Peer) acceptObject(msgType types.MessageType, obj types.Object, result 
 }
 
 func (p *Peer) resolvePendingBlocks(msgType types.MessageType, resolvedObjID types.HashID) {
+	
 	// Fetch and clear pending blocks waiting on this object
 	pendingBlocks := p.Manager.FetchPendingBlocks(resolvedObjID)
 
 	for _, pending := range pendingBlocks {
 		blk := pending.Block
 
-		// Re-validate the block now that the missing object has arrived
+		// Revalidate the block now that the missing object has arrived
 		result := p.Manager.ValidateObject(blk, p.addr)
 
 		if result.ErrorCode == types.E_NONE && result.Error == nil {
-			// BRANCH 1: Success! The block is now fully valid.
+			// The block is valid
 			p.log(msgType, types.E_NONE, fmt.Sprintf("Successfully validated pending block %s", result.ObjID))
 			p.acceptObject(msgType, blk, result)
 
 		} else if result.ErrorCode == types.E_UNKNOWN_OBJECT {
-			// BRANCH 2: Still missing objects (e.g., the block was missing multiple txs)
+			// Still missing objects (e.g., the block was missing multiple txs)
 			// We DO NOT send an error here. We just ask for the next missing piece.
 			p.log(msgType, types.E_NONE, fmt.Sprintf("Pending block still missing objects. Asking for %s", result.MissingID))
 			if result.MissingID != types.DUMMY_HASH {
@@ -57,8 +63,7 @@ func (p *Peer) resolvePendingBlocks(msgType types.MessageType, resolvedObjID typ
 			}
 
 		} else {
-			// BRANCH 3: Consensus Failure (e.g., E_INVALID_TX_OUTPOINT)
-			// The block mathematically failed. We MUST broadcast this error to the network!
+			// Invalid block
 			p.err(msgType, types.E_NONE, fmt.Sprintf("Pending block %s remains invalid: %v", result.ObjID, result.Error))
 			p.SendError(result.ErrorCode, result.Error.Error())
 		}
