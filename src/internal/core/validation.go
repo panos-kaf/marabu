3f914f4fbfdfe9843188ec53662a6b051ca47990
Panos Kaf <panoskatsalifis@gmail.com>
2026-05-08 00:49:35 +0300

    purify validation functions and use ValidationResult struct

--- src/internal/core/validation.go
+++ src/internal/core/validation.go
@@ -25,63 +25,43 @@ type ValidationResult struct {
 	NewHeight  uint64
 }
 
-func (m *Manager) ValidateObject(obj types.Object, peerAddr string) ValidationResult {
-	result := ValidationResult{
-		ObjID:     types.DUMMY_HASH,
-		Fee:       types.ZERO_PICABU,
-		ErrorCode: types.E_NONE,
-		Error:     nil,
-		MissingID: types.DUMMY_HASH,
-	}
+func (m *Manager) ValidateObject(obj types.Object) ValidationResult {
+
 	objIDstr, err := crypto.HashObject(obj)
 	if err != nil {
-		result.ErrorCode = types.E_INTERNAL_ERROR
-		result.Error = fmt.Errorf("Failed to hash object for validation: %v", err)
-		return result
+		return ValidationResult{ErrorCode: types.E_INTERNAL_ERROR, Error: fmt.Errorf("Failed to hash object: %v", err)}
 	}
+
 	objID := types.HashID(objIDstr)
+
+	var result ValidationResult
 
 	switch o := obj.(type) {
 	case *types.Transaction:
-		fee, errorCode, err := m.ValidateTransaction(o)
-		result.ObjID = objID
-		result.Fee = fee
-		result.ErrorCode = errorCode
-		result.Error = err
-		return result
+		result = m.ValidateTransaction(o)
 
 	case *types.CoinbaseTransaction:
-		fee, errorCode, err := m.ValidateCoinbase(o)
-		result.ObjID = objID
-		result.Fee = fee
-		result.ErrorCode = errorCode
-		result.Error = err
-		return result
+		result = m.ValidateCoinbase(o)
 
 	case *types.Block:
-		errorCode, missingID, newUTXO, isNewTip, newHeight, err := m.ValidateBlock(o, peerAddr)
-
-		result.ObjID = objID
-		result.ErrorCode = errorCode
-		result.MissingID = missingID
-		result.Error = err
-
-		result.NewUTXOSet = newUTXO
-		result.IsNewTip = isNewTip
-		result.NewHeight = newHeight
-
-		return result
+		result = m.ValidateBlock(o)
 
 	default:
-		result.ErrorCode = types.E_INTERNAL_ERROR
-		result.Error = fmt.Errorf("Unknown object type: %T", obj)
-		return result
+		return ValidationResult{ErrorCode: types.E_INTERNAL_ERROR, Error: fmt.Errorf("Unknown object type: %T", obj)}
 	}
+
+	result.ObjID = objID
+	return result
 }
 
-func (m *Manager) ValidateTransaction(tx *types.Transaction) (types.Picabu, types.ErrorCode, error) {
+func (m *Manager) ValidateTransaction(tx *types.Transaction) ValidationResult {
+
+	result := ValidationResult{ObjID: types.DUMMY_HASH, Fee: types.ZERO_PICABU, ErrorCode: types.E_NONE, Error: nil}
+
 	if tx.Type != types.OBJ_TRANSACTION {
-		return types.ZERO_PICABU, types.E_INTERNAL_ERROR, fmt.Errorf("Invalid object type for transaction: %s", tx.Type)
+		result.ErrorCode = types.E_INTERNAL_ERROR
+		result.Error = fmt.Errorf("Invalid object type for transaction: %s", tx.Type)
+		return result
 	}
 
 	sumInputs := new(big.Int)
@@ -101,15 +81,21 @@ func (m *Manager) ValidateTransaction(tx *types.Transaction) (types.Picabu, type
 
 		key := OutpointKey{Txid: outpoint.Txid, Index: idx}
 		if m.IsInputSpent(key) {
-			return types.ZERO_PICABU, types.E_INVALID_TX_OUTPOINT, fmt.Errorf("Input %d is already being spent by another transaction in the mempool", i)
+			result.ErrorCode = types.E_INVALID_TX_OUTPOINT
+			result.Error = fmt.Errorf("Input %d is already being spent by another transaction in the mempool", i)
+			return result
 		}
 
 		obj, err := m.db.getObject(outpoint.Txid)
 		if err != nil {
 			if errors.Is(err, ErrNotFound) {
-				return types.ZERO_PICABU, types.E_UNKNOWN_OBJECT, fmt.Errorf("Referenced transaction %s for input %d does not exist", outpoint.Txid, i)
+				result.ErrorCode = types.E_UNKNOWN_OBJECT
+				result.Error = fmt.Errorf("Referenced transaction %s for input %d does not exist", outpoint.Txid, i)
+				return result
 			}
-			return types.ZERO_PICABU, types.E_UNKNOWN_OBJECT, fmt.Errorf("Failed to fetch referenced transaction")
+			result.ErrorCode = types.E_UNKNOWN_OBJECT
+			result.Error = fmt.Errorf("Failed to fetch referenced transaction")
+			return result
 		}
 
 		var outputs []types.TxOutput
@@ -119,16 +105,22 @@ func (m *Manager) ValidateTransaction(tx *types.Transaction) (types.Picabu, type
 		case *types.CoinbaseTransaction:
 			outputs = txObj.Outputs
 		default:
-			return types.ZERO_PICABU, types.E_INTERNAL_ERROR, fmt.Errorf("Referenced object is of unknown type")
+			result.ErrorCode = types.E_INTERNAL_ERROR
+			result.Error = fmt.Errorf("Referenced object is of unknown type")
+			return result
 		}
 
 		if idx < 0 || idx >= len(outputs) {
-			return types.ZERO_PICABU, types.E_INVALID_TX_OUTPOINT, fmt.Errorf("Invalid output index")
+			result.ErrorCode = types.E_INVALID_TX_OUTPOINT
+			result.Error = fmt.Errorf("Invalid output index")
+			return result
 		}
 
 		// Check for duplicate outpoints
 		if outpoints[key] {
-			return types.ZERO_PICABU, types.E_INVALID_TX_OUTPOINT, fmt.Errorf("Multiple inputs have the same outpoint.")
+			result.ErrorCode = types.E_INVALID_TX_OUTPOINT
+			result.Error = fmt.Errorf("Multiple inputs have the same outpoint.")
+			return result
 		}
 		outpoints[key] = true
 
@@ -136,7 +128,9 @@ func (m *Manager) ValidateTransaction(tx *types.Transaction) (types.Picabu, type
 		sumInputs.Add(sumInputs, (*big.Int)(output.Value))
 
 		if input.Sig == nil {
-			return types.ZERO_PICABU, types.E_INVALID_TX_SIGNATURE, fmt.Errorf("Missing signature")
+			result.ErrorCode = types.E_INVALID_TX_SIGNATURE
+			result.Error = fmt.Errorf("Missing signature")
+			return result
 		}
 
 		verifyQueue = append(verifyQueue, sigData{
@@ -150,33 +144,46 @@ func (m *Manager) ValidateTransaction(tx *types.Transaction) (types.Picabu, type
 	}
 
 	if sumOutputs.Cmp(sumInputs) == 1 {
-		return types.ZERO_PICABU, types.E_INVALID_TX_CONSERVATION, fmt.Errorf("Output value %d exceeds input value %d", sumOutputs, sumInputs)
+		result.ErrorCode = types.E_INVALID_TX_CONSERVATION
+		result.Error = fmt.Errorf("Output value %d exceeds input value %d", sumOutputs, sumInputs)
+		return result
 	}
 
 	msg := serialization.TxMessageForSignature(tx)
 	for i, data := range verifyQueue {
 		if !crypto.Verify(data.pubkey, msg, data.sig) {
-			return types.ZERO_PICABU, types.E_INVALID_TX_SIGNATURE, fmt.Errorf("Invalid signature for input %d", i)
+			result.ErrorCode = types.E_INVALID_TX_SIGNATURE
+			result.Error = fmt.Errorf("Invalid signature for input %d", i)
+			return result
 		}
 	}
 
 	fee := new(big.Int).Sub(sumInputs, sumOutputs)
-	return types.Picabu(*fee), types.E_NONE, nil
+	result.ErrorCode = types.E_NONE
+	result.Fee = types.Picabu(*fee)
+	return result
 }
 
-func (m *Manager) ValidateCoinbase(cb *types.CoinbaseTransaction) (types.Picabu, types.ErrorCode, error) {
-	return types.ZERO_PICABU, types.E_NONE, nil
+func (m *Manager) ValidateCoinbase(cb *types.CoinbaseTransaction) ValidationResult {
+	return ValidationResult{ErrorCode: types.E_NONE, Fee: types.ZERO_PICABU}
 }
 
-func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorCode, types.HashID, *UTXOSet, bool, uint64, error) {
+func (m *Manager) ValidateBlock(blk *types.Block) ValidationResult {
+
+	result := ValidationResult{ErrorCode: types.E_NONE}
+
 	now := time.Now().Unix()
 	if int64(blk.Created) > now {
-		return types.E_INVALID_BLOCK_TIMESTAMP, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Block timestamp %d comes from the future :o", blk.Created)
+		result.ErrorCode = types.E_INVALID_BLOCK_TIMESTAMP
+		result.Error = fmt.Errorf("Block timestamp %d comes from the future :o", blk.Created)
+		return result
 	}
 
 	blockid, err := crypto.HashObject(blk)
 	if err != nil {
-		return types.E_INTERNAL_ERROR, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Failed to hash block for validation: %v", err)
+		result.ErrorCode = types.E_INTERNAL_ERROR
+		result.Error = fmt.Errorf("Failed to hash block for validation: %v", err)
+		return result
 	}
 
 	isValid, err := crypto.VerifyPoW(blockid)
@@ -185,10 +192,14 @@ func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorC
 	// isValid = true // TEMPORARY: Disable PoW verification for testing
 
 	if err != nil {
-		return types.E_INTERNAL_ERROR, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Failed to verify PoW for block %s: %v", blockid, err)
+		result.ErrorCode = types.E_INTERNAL_ERROR
+		result.Error = fmt.Errorf("Failed to verify PoW for block %s: %v", blockid, err)
+		return result
 	}
 	if !isValid {
-		return types.E_INVALID_BLOCK_POW, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Invalid PoW for block %s", blockid)
+		result.ErrorCode = types.E_INVALID_BLOCK_POW
+		result.Error = fmt.Errorf("Invalid PoW for block %s", blockid)
+		return result
 	}
 
 	isGenesis := blk.Previd == nil
@@ -196,15 +207,21 @@ func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorC
 		prevObj, err := m.GetObject(*blk.Previd)
 		if err != nil {
 			if errors.Is(err, ErrNotFound) {
-				m.db.addPendingBlock(peerAddr, *blk.Previd, blk)
-				return types.E_UNKNOWN_OBJECT, *blk.Previd, nil, false, 0, fmt.Errorf("Parent block %s not found. Asked peers for it", *blk.Previd)
+				result.ErrorCode = types.E_UNKNOWN_OBJECT
+				result.MissingID = *blk.Previd
+				result.Error = fmt.Errorf("Parent block %s not found. Asked peers for it", *blk.Previd)
+				return result
 			}
-			return types.E_INTERNAL_ERROR, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Failed to fetch parent block %s: %v", *blk.Previd, err)
+			result.ErrorCode = types.E_INTERNAL_ERROR
+			result.Error = fmt.Errorf("Failed to fetch parent block %s: %v", *blk.Previd, err)
+			return result
 		}
 		prevBlock := prevObj.(*types.Block)
 
 		if blk.Created <= prevBlock.Created {
-			return types.E_INVALID_BLOCK_TIMESTAMP, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Block timestamp %d is earlier than its parent's timestamp %d", blk.Created, prevBlock.Created)
+			result.ErrorCode = types.E_INVALID_BLOCK_TIMESTAMP
+			result.Error = fmt.Errorf("Block timestamp %d is earlier than its parent's timestamp %d", blk.Created, prevBlock.Created)
+			return result
 		}
 	}
 
@@ -213,13 +230,18 @@ func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorC
 
 	if isGenesis {
 		if blockid != genesisHash {
-			return types.E_INVALID_GENESIS, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("invalid genesis block")
+			result.ErrorCode = types.E_INVALID_GENESIS
+			result.Error = fmt.Errorf("invalid genesis block")
+			return result
 		}
 		height = 0
 	} else {
 		UTXO, err := m.db.getUTXO(*blk.Previd)
 		if err != nil {
-			return types.E_UNKNOWN_OBJECT, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Could not find UTXO for block %s: %v", *blk.Previd, err)
+			result.ErrorCode = types.E_UNKNOWN_OBJECT
+			result.MissingID = *blk.Previd
+			result.Error = fmt.Errorf("Could not find UTXO for block %s: %v", *blk.Previd, err)
+			return result
 		}
 		utxos = UTXO.UTXOs
 		height = UTXO.Height + 1
@@ -234,11 +256,15 @@ func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorC
 		tx, err := m.GetObject(txid)
 		if err != nil {
 			if errors.Is(err, ErrNotFound) {
-				m.db.addPendingBlock(peerAddr, txid, blk)
-				return types.E_UNKNOWN_OBJECT, txid, nil, false, 0, fmt.Errorf("Block references transactions we don't have")
+				result.ErrorCode = types.E_UNKNOWN_OBJECT
+				result.MissingID = txid
+				result.Error = fmt.Errorf("Block references transactions we don't have")
+				return result
 			}
 
-			return types.E_INTERNAL_ERROR, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Failed to fetch referenced transaction: %v", err)
+			result.ErrorCode = types.E_INTERNAL_ERROR
+			result.Error = fmt.Errorf("Failed to fetch referenced transaction: %v", err)
+			return result
 		}
 		switch t := tx.(type) {
 		case *types.Transaction:
@@ -247,12 +273,16 @@ func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorC
 
 			for _, input := range t.Inputs {
 				if hasCoinbase && input.Outpoint.Txid == cbID {
-					return types.E_INVALID_TX_OUTPOINT, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Cannot spend coinbase transaction in the same block")
+					result.ErrorCode = types.E_INVALID_TX_OUTPOINT
+					result.Error = fmt.Errorf("Cannot spend coinbase transaction in the same block")
+					return result
 				}
 				key := OutpointKey{Txid: input.Outpoint.Txid, Index: int(*input.Outpoint.Index)}
 				spentOutput, exists := utxos[key]
 				if !exists {
-					return types.E_INVALID_TX_OUTPOINT, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Invalid input: %s (not found in UTXO set)", txid)
+					result.ErrorCode = types.E_INVALID_TX_OUTPOINT
+					result.Error = fmt.Errorf("Invalid input: %s (not found in UTXO set)", txid)
+					return result
 				}
 				sumIn.Add(sumIn, (*big.Int)(spentOutput.Value))
 				delete(utxos, key)
@@ -270,14 +300,20 @@ func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorC
 			cbID = types.HashID(id)
 
 			if index != 0 {
-				return types.E_INVALID_BLOCK_COINBASE, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Only the first transaction in the block can be a coinbase, found one at index %d", index)
+				result.ErrorCode = types.E_INVALID_BLOCK_COINBASE
+				result.Error = fmt.Errorf("Only the first transaction in the block can be a coinbase, found one at index %d", index)
+				return result
 			}
 			if uint64(*t.Height) != height {
-				return types.E_INVALID_BLOCK_COINBASE, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Coinbase transaction has height %d, while its block has height %d", *t.Height, height)
+				result.ErrorCode = types.E_INVALID_BLOCK_COINBASE
+				result.Error = fmt.Errorf("Coinbase transaction has height %d, while its block has height %d", *t.Height, height)
+				return result
 			}
 			utxos[OutpointKey{Txid: cbID, Index: 0}] = t.Outputs[0]
 		default:
-			return types.E_INTERNAL_ERROR, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Referenced object is of unknown type")
+			result.ErrorCode = types.E_INTERNAL_ERROR
+			result.Error = fmt.Errorf("Referenced object is of unknown type")
+			return result
 		}
 	}
 
@@ -288,11 +324,13 @@ func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorC
 
 	totalOutput := new(big.Int).Add(types.BlockRewardBigInt(), fees)
 	if coinbaseVal.Cmp(totalOutput) == 1 {
-		return types.E_INVALID_BLOCK_COINBASE, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Coinbase transaction value %d exceeds allowed reward+fees of %d", coinbaseVal, totalOutput)
+		result.ErrorCode = types.E_INVALID_BLOCK_COINBASE
+		result.Error = fmt.Errorf("Coinbase transaction value %d exceeds allowed reward+fees of %d", coinbaseVal, totalOutput)
+		return result
 	}
 
 	// Build the proposed UTXO set, but DO NOT save it
-	newSet := UTXOSet{UTXOs: utxos, BlockID: types.HashID(blockid), Height: height}
+	result.NewUTXOSet = &UTXOSet{UTXOs: utxos, BlockID: types.HashID(blockid), Height: height}
 
 	var curHeight uint64
 	hasTip := true
@@ -305,7 +343,9 @@ func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorC
 			hasTip = false
 		} else {
 			// database error
-			return types.E_INTERNAL_ERROR, types.DUMMY_HASH, nil, false, 0, fmt.Errorf("Failed to get current chaintip for block validation: %v", err)
+			result.ErrorCode = types.E_INTERNAL_ERROR
+			result.Error = fmt.Errorf("Failed to get current chaintip for block validation: %v", err)
+			return result
 		}
 	}
 
@@ -313,5 +353,9 @@ func (m *Manager) ValidateBlock(blk *types.Block, peerAddr string) (types.ErrorC
 	isNewTip := !hasTip || height > curHeight
 
 	// Return the proposed state instead of saving it
-	return types.E_NONE, types.DUMMY_HASH, &newSet, isNewTip, height, nil
+	result.ErrorCode = types.E_NONE
+	result.NewHeight = height
+	result.IsNewTip = isNewTip
+	result.Error = nil
+	return result
 }
