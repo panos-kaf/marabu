090436c429d316055a78cfac5a0e07ab0f9fb3bc
Panos Kaf <panoskatsalifis@gmail.com>
2026-05-08 00:50:41 +0300

    delegate state logic to core and add reject object method

--- src/internal/peer/objects.go
+++ src/internal/peer/objects.go
@@ -6,58 +6,45 @@ import (
 	"marabu/internal/types"
 )
 
-func (p *Peer) acceptObject(msgType types.MessageType, obj types.Object, result core.ValidationResult) {
+func (p *Peer) acceptObject(obj types.Object, result core.ValidationResult) {
 
-	// Deduplication check
-	exists, err := p.Manager.ExistsObject(result.ObjID)
-	if err != nil {
-		p.err(msgType, types.E_NONE, "DB Error: "+err.Error())
-		return
-	}
+	exists, _ := p.Manager.ExistsObject(result.ObjID)
 	if exists {
-		p.log(msgType, types.E_NONE, "Already have object "+string(result.ObjID)+", ignoring.")
 		return
 	}
 
-	isMempoolConflict := false
-	if tx, ok := obj.(*types.Transaction); ok {
-		for _, input := range tx.Inputs {
-			outpoint := core.OutpointKey{Txid: input.Outpoint.Txid, Index: int(*input.Outpoint.Index)}
-			if p.Manager.IsInputSpent(outpoint) {
-				isMempoolConflict = true
-				break
-			}
-		}
-
-		// If it's a conflict and we don't need it for a block, drop it
-		if isMempoolConflict && !p.Manager.IsNeededForPendingBlock(result.ObjID) {
-			p.err(msgType, types.E_INVALID_TX_OUTPOINT, "Mempool double-spend detected.")
-			return
-		}
-	}
-
-	// Store the object and apply state changes
-	if err := p.Manager.CommitObject(obj, result); err != nil {
-		p.err(msgType, types.E_NONE, "Failed to apply state: "+err.Error())
+	gossip, errorCode, err := p.Manager.CommitObject(obj, result)
+	if err != nil {
+		p.err(types.MSG_OBJECT, errorCode, "Failed to apply state: "+err.Error())
 		return
 	}
-	p.logInfo(fmt.Sprintf("Successfully processed and stored %s", result.ObjID))
 
-	if tx, ok := obj.(*types.Transaction); ok {
+	if errorCode != types.E_NONE {
+		p.SendError(errorCode, "Transaction stored, but rejected from mempool.")
+	}
 
-		// Only add to mempool and gossip if it ISN'T a conflict
-		if !isMempoolConflict {
-			p.Manager.AddToMempool(tx, result.Fee)
-			BroadcastIHaveObject(result.ObjID)
-		} else {
-			p.logInfo(fmt.Sprintf("Tx %s saved for pending block, but withheld from mempool due to conflict.", result.ObjID))
-		}
-	} else {
-		// gossip
+	if gossip {
 		BroadcastIHaveObject(result.ObjID)
 	}
-	// Resolve pending blocks that depended on this object
-	p.resolvePendingBlocks(msgType, result.ObjID)
+
+	p.resolvePendingBlocks(types.MSG_OBJECT, result.ObjID)
+}
+
+func (p *Peer) rejectObject(obj types.Object, result core.ValidationResult) {
+	if result.ErrorCode == types.E_UNKNOWN_OBJECT && result.MissingID != types.DUMMY_HASH {
+
+		p.log(types.MSG_OBJECT, types.E_NONE, fmt.Sprintf("Missing objects. Pausing block and requesting %s", result.MissingID))
+
+		if blk, ok := obj.(*types.Block); ok {
+			p.Manager.AddPendingBlock(p.addr, result.MissingID, blk)
+		}
+
+		BroadcastGetObject(result.MissingID)
+		return
+	}
+
+	p.err(types.MSG_OBJECT, result.ErrorCode, result.Error.Error())
+	p.SendError(result.ErrorCode, result.Error.Error())
 }
 
 func (p *Peer) resolvePendingBlocks(msgType types.MessageType, resolvedObjID types.HashID) {
@@ -69,12 +56,12 @@ func (p *Peer) resolvePendingBlocks(msgType types.MessageType, resolvedObjID typ
 		blk := pending.Block
 
 		// Revalidate the block now that the missing object has arrived
-		result := p.Manager.ValidateObject(blk, p.addr)
+		result := p.Manager.ValidateObject(blk)
 
 		if result.ErrorCode == types.E_NONE && result.Error == nil {
 			// The block is valid
 			p.log(msgType, types.E_NONE, fmt.Sprintf("Successfully validated pending block %s", result.ObjID))
-			p.acceptObject(msgType, blk, result)
+			p.acceptObject(blk, result)
 
 		} else if result.ErrorCode == types.E_UNKNOWN_OBJECT {
 			// Still missing objects (e.g., the block was missing multiple txs)
