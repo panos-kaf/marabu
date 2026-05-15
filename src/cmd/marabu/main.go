2418830d9140c94af6606d62f433fc4a306d5061
Panos Kaf <panoskatsalifis@gmail.com>
2026-05-15 22:02:46 +0300

    gen sk, gen pk and pass it to miner. mkdir for db

--- src/cmd/marabu/main.go
+++ src/cmd/marabu/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/ed25519"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"marabu/internal/bootstrap"
 	"marabu/internal/core"
+	"marabu/internal/crypto"
 	"marabu/internal/logs"
+	"marabu/internal/miner"
 	"marabu/internal/peer"
 	"marabu/internal/types"
 	"marabu/internal/ui"
@@ -30,6 +34,12 @@ func main() {
 	logFile := logs.InitLogs()
 	defer logFile.Close()
 
+	err := os.MkdirAll("./db", 0755)
+	if err != nil {
+		fmt.Printf("Error creating db directory: %v\n", err)
+		os.Exit(1)
+	}
+
 	PEERS_FILE := filepath.Join(".", "db", "peers.csv")
 	peersFile, err := os.OpenFile(PEERS_FILE, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
@@ -51,6 +61,18 @@ func main() {
 	go Manager.SyncNodeState(peer.BroadcastGetMempool)
 
 	go peer.StartServer(Manager)
+
+	privKey, err := crypto.LoadOrGenerateKey("db/node.priv")
+	if err != nil {
+		panic("Failed to load node keys!")
+	}
+
+	// Extract the public key and cast it to your HashID type
+	pubKeyBytes := privKey.Public().(ed25519.PublicKey)
+	myPubKey := types.HashID(hex.EncodeToString(pubKeyBytes))
+
+	Miner := miner.NewMiner(Manager, myPubKey)
+	go Miner.StartMining()
 
 	bootstrap.StartNode(Manager)
 	ui.Start(Manager)
